3469ffe8ea128304731601f2f21c630557077c19
irem <iremhverdi@gmail.com>
2024-10-28 20:44:43 +0300

    Added if statements for invalid moves Added various 'clear' commands to make the output in the console more readable and cleaner

--- tictac.java
+++ tictac.java
@@ -5,19 +5,32 @@ public class tictac{
     static int cols=3;
     static char[][] board= new char[rows][cols];//new is required to determine the memory to be allocated.
     static char currentPlayer= 'X';
+    static int movementCounter=0;//to check how many moves are made
     public static void main(String[] args){
         initializeBoard();
         while(true){//If there are cells that not empty user should make a move
-            //clear console
             printBoard();
             System.out.println(currentPlayer+"'s turn. Where do you want the character?");//coordinates for the movement
             //movement should be in a function
             int newCoordinatx= input.nextInt();
             int newCoordinaty= input.nextInt();
-            if (isempty(newCoordinatx,newCoordinaty)&& newCoordinatx>=0 &&newCoordinatx<3 &&newCoordinaty>=0 &&newCoordinaty<3){
+            if(newCoordinatx<0 ||newCoordinatx>2 ||newCoordinaty<0 ||newCoordinaty>2){
+                System.out.println("\033[H\033[2J");//clear console command
+                System.out.println("This move can not be done. Coordinates should be in range 0-2");
+            }
+            else if(!isempty(newCoordinatx,newCoordinaty)){
+                System.out.println("\033[H\033[2J");//clear console command
+                System.out.println("This move can not be done. Choose an empty cell to move");
+            }
+            else{
+            System.out.println("\033[H\033[2J");//clear console command
             board[newCoordinatx][newCoordinaty]=currentPlayer;//if this cell is empty the move can be done
+            movementCounter++;
                 if(winner()){
+                    System.out.println("\033[H\033[2J");//clear console command
+                    printBoard();
                     System.out.println("Game is over. The winner is "+ currentPlayer);
+                    System.out.println(movementCounter+" moves made");
                     break;
                 }
                 else if (noMoveLeft()){
@@ -27,9 +40,6 @@ public class tictac{
                 else{
                 currentPlayer= (currentPlayer=='X')?'O':'X';//changing players
                 }
-            }
-            else{
-                System.out.println("This move can not be done. Choose an empty place to move");
             }
         }
     }
